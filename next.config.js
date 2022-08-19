17b805d587df1ac853864a6a754b74d84820da92
Tauseef Nafee Fattah <57330415+TauseefNafeeFattah@users.noreply.github.com>
2022-08-18 20:23:06 -0600

    change nextjs config file for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images:{
     domains:['yt3.ggpht.com',
       'lh3.googleusercontent.com',
